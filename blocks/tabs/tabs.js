f04d86e2963cebeadd19f631d71379aa28a3f91c
jaytech989 <jay.vyas@techchefz.com>
2025-06-06 17:43:38 +0530

    tab added

---
+++ blocks/tabs/tabs.js
@@ -0,0 +1,21 @@
+// const buttons = document.querySelectorAll(".tab-button");
+//   const contents = document.querySelectorAll(".tab-content");
+
+//   buttons.forEach(button => {
+//     button.addEventListener("click", () => {
+//       // Remove active classes
+//       buttons.forEach(btn => btn.classList.remove("active"));
+//       contents.forEach(content => content.classList.remove("active"));
+
+//       // Add active classes to selected tab and content
+//       button.classList.add("active");
+//       document.getElementById(button.dataset.tab).classList.add("active");
+//     });
+//   });
+
+
+  export default function decorate(block) {
+
+
+    console.log(block)
+  }
